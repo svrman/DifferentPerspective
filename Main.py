63730e1317734de5351be35c8bda631c83a95eda
pk <romanlandlord99@gmail.com>
2020-07-29 20:08:23 +0500

    Modified

--- Main.py
+++ Main.py
@@ -1,15 +1,30 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('1.png')
-width, height = 250, 350
-pts1 = np.float32([[123, 17], [5, 225], [177, 297], [285, 77]])
-pts2 = np.float32([[0,0],[width,0],[0,height],[width,height]])
-matrix = cv2.getPerspectiveTransform(pts1, pts2)
-imgOutput = cv2.warpPerspective(img, matrix, (width, height))
-for x in range(0, 4):
-    cv2.circle(img, (pts1[x][0], pts1[x][1]), 15, (0, 0, 255), cv2.FILLED)
+circles = np.zeros((4, 2), np.int)
+counter = 0
 
-cv2.imshow('Original', img)
-cv2.imshow('New', imgOutput)
-cv2.waitKey(0)
+
+def mousePoints(event, x, y, flags, params):
+    global counter
+    if event == cv2.EVENT_LBUTTONDOWN:
+        circles[counter] = x, y
+        counter = counter + 1
+        print(circles)
+
+
+img = cv2.imread('1.png')
+while True:
+    if counter == 4:
+        width, height = 250, 350
+        pts1 = np.float32([circles[0], circles[1], circles[2], circles[3]])
+        pts2 = np.float32([[0, 0], [width, 0], [0, height], [width, height]])
+        matrix = cv2.getPerspectiveTransform(pts1, pts2)
+        imgOutput = cv2.warpPerspective(img, matrix, (width, height))
+        cv2.imshow("Output Image ", imgOutput)
+    for x in range(0, 4):
+        cv2.circle(img, (circles[x][0], circles[x][1]), 3, (0, 255, 0), cv2.FILLED)
+
+    cv2.imshow("Original Image ", img)
+    cv2.setMouseCallback("Original Image ", mousePoints)
+    cv2.waitKey(1)
